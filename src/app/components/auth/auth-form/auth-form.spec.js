e19832abb395c2efed7bb9fe05023a4a628a4d8c
Dirk Manske <manske@DOMAIN.SWL>
2017-10-04 13:48:45 +0200

    auth-form impl

---
+++ src/app/components/auth/auth-form/auth-form.spec.js
@@ -0,0 +1,29 @@
+describe('Auth', () => {
+    beforeEach(() => {
+        angular.mock.module('components.auth');
+    });
+
+    describe('AuthFormController', () => {
+        let $componentController;
+        let controller;
+        const mockUser = { $id: 1 };
+        const mockSubmit = angular.noop;
+
+        beforeEach(inject(($injector) => {
+            $componentController = $injector.get('$componentController');
+            
+            controller = $componentController('authForm', 
+                { $scope: {} },
+                {   user: mockUser, button: '', message: '', onSubmit: mockSubmit }
+            );
+        }));
+
+        it('should call onSubmit with the correct payload', () => {
+            const payload = { $event: { user: mockUser } };
+            
+            spyOn(controller, 'onSubmit');
+            controller.submitForm();
+            expect(controller.onSubmit).toHaveBeenCalledWith(payload);
+        });
+    });
+});
